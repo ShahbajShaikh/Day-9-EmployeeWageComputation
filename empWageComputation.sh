ab75f3e536a57deb4de9b9d7234a9b5aa15b516a
shahbaj <shahbaj.49408@gmail.com>
2020-10-25 00:14:32 +0530

    Added: Part Time UC3

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -23,3 +23,22 @@ else
 fi
 
 salary=$(( $empWagePerHr * $empHr ))
+
+empWagePerHr=20;
+Random=$((RANDOM%2))
+
+if [ $Random -eq 0 ]
+then
+        echo "Employee is present";
+        empHr=12;
+        elif [ $Random -eq 1 ]
+        then
+                echo "Employee is part time present.";
+                empHr=8;
+        else
+                echo "Employee Absent.";
+                empHr=0;
+        fi
+
+salary=$(( $empWagePerHr * $empHr ))
+echo $salary
